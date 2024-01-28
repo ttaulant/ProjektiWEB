5cdd969851ed6fb19cdb3aebac08a7b469eecfe2
ttaulant <tm67327@ubt-uni.net>
2024-01-29 00:12:51 +0100

    added icon and app name

--- login.php
+++ login.php
@@ -4,22 +4,28 @@
 
         <title>Log In</title>
         <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/dabac98fff.js" crossorigin="anonymous"></script>
 
     </head>
     <body>
 
         <div class="loginMain">
-            <form id="loginForm" action="validate.php" method="post">
-                    <h2> Welcome </h2>
-                    <label for="username">Username:</label>
-                    <input type="text" id="username" name="username" required>
-                    <label for="password">Password:</label>
-                    <input type="password" id="password" name="password" required>
-                    <button id="loginButton" type="submit" name="login">Log  In</button>
-                    <p>You don't have an account?</p>
-                    <a href="signup.php">Sign Up</a>
+            <div>
+                <i class="fa-solid fa-paw fa-2xl"></i>   
+                <h1>Animal Event</h1>         
+            </div>
+            <div>
+                <form id="loginForm" action="validate.php" method="post">
+                        <label for="username">Username:</label>
+                        <input type="text" id="username" name="username" required>
+                        <label for="password">Password:</label>
+                        <input type="password" id="password" name="password" required>
+                        <button id="loginButton" type="submit" name="login">Log  In</button>
+                        <p>You don't have an account?</p>
+                        <a href="signup.php">Sign Up</a>
 
-            </form>
+                </form>
+            </div>
         </div>
         <p class="error" id="error"></p>
 
